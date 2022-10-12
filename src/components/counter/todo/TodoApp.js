6040fa8b3102007b7570eb63cd5628c4dd37dde6
fahim25 <fahimmuntasir201@gmail.com>
2022-10-13 00:20:24 +0600

    worked on editable field

--- src/components/counter/todo/TodoApp.js
+++ src/components/counter/todo/TodoApp.js
@@ -1,4 +1,5 @@
-import React, { useState }from "react"
+import React from "react"
+import { Component } from "react";
 
 
 const TodoApp = () => {
@@ -9,17 +10,50 @@ const TodoApp = () => {
       )
 }
 
-const LogIn = (props) =>{
+class LogIn extends Component{
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      userName: 'username',
+      password: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+
+    //static
+
+    // this.handleUserName = this.handleUserName.bind(this);
+    // this.handlePassword = this.handlePassword.bind(this);
+  };
 
   
+  handleChange(event){
+    this.setState({
+        [event.target.name]
+        :event.target.value
+    })
+  }
 
-  return(
-     <>
-        User Name: <input type="text" name="userName" value={this.state.userName} />
-        Password: <input type="password" name="password" />
-        <button>Log In</button>
-     </>
-  )
+  //static 
+
+  // handleUserName(event){
+  //   this.setState({userName:event.target.value})
+  // }
+  // handlePassword(event){
+  //   this.setState({password:event.target.value})
+  // }
+
+  render(){
+    return(
+      <>
+         User Name: <input type="text" name="userName" value={this.state.userName} onChange={this.handleChange} />
+         Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
+         <button>Log In</button>
+      </>
+   )
+  }
 }
 
 export { TodoApp, LogIn }
