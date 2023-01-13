82334ed764baf1367727fd59039e800f204d8e6d
fahim25 <fahimmuntasir201@gmail.com>
2023-01-13 23:07:30 +0600

    working with api

--- src/components/counter/todo/Welcome.js
+++ src/components/counter/todo/Welcome.js
@@ -1,13 +1,35 @@
+import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import HelloWorldService from "../../../api/todo/HelloWorldService";
 
 
 function WelcomeComp() {
     let { name } = useParams();
+
+    let [welconmeMessage ,setWelcomeMessage] = useState('');
+
+    function getMessage(){
+      HelloWorldService.executeHelloWorldService()
+      .then(response => setSuccesResponse(response))
+      // .catch()
+    }
+
+    function setSuccesResponse(response){
+      setWelcomeMessage(response.data)
+    }
+
     return (
-      <div>
-        <h4>Welcome {name}!!</h4>
-        <p>You can manage your todos <Link to="/todolist">here</Link></p>
-      </div>
+      <>
+        <div>
+          <h4>Welcome {name}!!</h4>
+          <p>You can manage your todos <Link to="/todolist">here</Link></p>
+        </div>
+        <div>
+          <h4>Get Welcome Message from Spring Boot.</h4>
+          <button onClick={getMessage()} className="btn btn-info" >Get Message</button>
+          <p>{welconmeMessage}</p>
+        </div>
+      </>
     )
 }
 
